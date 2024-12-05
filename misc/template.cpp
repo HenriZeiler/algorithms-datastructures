1cbfdcca5f29fd9966b85944516d3ce16d8a3de6
HenriZeiler <henri.zeiler02@outlook.de>
2024-12-05 22:03:27 +0100

    add template

---
+++ misc/template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#define rep(a, b) for (int a = 0; a < (b); ++a)
+#define all(a) (a).begin(), (a).end()
+#define endl '\n'
+#define pb push_back
+#define eb emplace_back
+
+using namespace std;
+using ii = pair<int, int>;
+using cint = const int;
+using vii = vector<ii>;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vvi = vector<vector<int> >;
+using ll = long long;
+using vll = vector<ll>;
+using Graph = vector<vector<int> >;
+const int MOD = 1000000007;
+const ll INF = 9223372036854775807;
+const int iINF = 0x3f3f3f3f;
